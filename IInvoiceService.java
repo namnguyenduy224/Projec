7368fee97a3fc2151b551d03f2dd5ada69806e0e
truzvng0418 <quoctrung0418@gmail.com>
2026-07-06 20:04:34 +0700

    Delete IInvoiceService.java

--- IInvoiceService.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public interface IInvoiceService extends IBaseService<Invoice> {
-    Invoice createInvoice(TeaSession session);
-    double calculateTotal(Invoice invoice);
-    boolean payInvoice(String invoiceId);
-    List<Invoice> findUnpaidInvoices();
-}
