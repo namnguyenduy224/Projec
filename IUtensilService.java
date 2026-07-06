423cea71aead945e1a12ad7d5bb8c65aeb7d569b
truzvng0418 <quoctrung0418@gmail.com>
2026-07-06 20:03:04 +0700

    Delete IUtensilService.java

--- IUtensilService.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-
-public interface IUtensilService extends IBaseService<Utensil> {
-    List<Utensil> findByType(UtensilType type);
-    List<Utensil> findAvailableUtensils();
-    boolean checkQuantity(String utensilId, int quantity);
-}
