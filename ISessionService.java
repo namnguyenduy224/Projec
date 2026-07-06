7eb1775216a94fd9e863c3c3de61f9f82d091d69
truzvng0418 <quoctrung0418@gmail.com>
2026-07-06 20:04:11 +0700

    Delete ISessionService.java

--- ISessionService.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface ISessionService extends IBaseService<TeaSession> {
-    TeaSession createSession(Customer customer, Staff staff, Tea tea, Teapot teapot, List<Utensil> utensils);
-    boolean finishSession(String sessionId);
-    boolean cancelSession(String sessionId);
-    double calculateSessionTotal(String sessionId);
-    List<TeaSession> findByCustomer(String customerId);
-}
