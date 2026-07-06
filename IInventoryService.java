7cea06e4626c70a5a10c9e356bb6f0306b036656
truzvng0418 <quoctrung0418@gmail.com>
2026-07-06 20:02:51 +0700

    Delete IInventoryService.java

--- IInventoryService.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface IInventoryService {
-    boolean addStock(String itemId, int quantity);
-    boolean reduceStock(String itemId, int quantity);
-    boolean checkStock(String itemId, int quantity);
-    List<Inventory> getLowStockItems(int minQuantity);
-    List<Inventory> getAllInventory();
-}
