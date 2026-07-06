b33d1b325a8a8ac6e92d3243ca04c9d6bf088bbf
truzvng0418 <quoctrung0418@gmail.com>
2026-07-06 20:03:21 +0700

    Delete ITeaService.java

--- ITeaService.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface ITeaService extends IBaseService<Tea> {
-    List<Tea> findByType(TeaType type);
-    List<Tea> findLowStock(int minGram);
-    boolean importTea(String teaId, int gram);
-    boolean exportTea(String teaId, int gram);
-    double calculateTeaPrice(String teaId, int gram);
-}
