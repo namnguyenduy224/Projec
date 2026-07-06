2865dc48680c9db389356cc6909a1e9dcf3fb118
truzvng0418 <quoctrung0418@gmail.com>
2026-07-06 20:02:34 +0700

    Delete IExportService.java

--- IExportService.java
+++
@@ -1,5 +0,0 @@
-public interface IExportService {
-    boolean exportInvoiceToTxt(Invoice invoice, String filePath);
-    boolean exportStatisticToTxt(String filePath);
-    boolean exportInventoryToTxt(String filePath);
-}
