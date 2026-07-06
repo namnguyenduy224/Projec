bbfb6dc4e7db17a008a190c4fdb635a095eb7545
truzvng0418 <quoctrung0418@gmail.com>
2026-07-06 20:04:01 +0700

    Delete IStaffService.java

--- IStaffService.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-
-public interface IStaffService extends IBaseService<Staff> {
-    List<Staff> findByRole(StaffRole role);
-    List<Staff> findByStatus(StaffStatus status);
-    List<Staff> findActiveStaff();
-}
