d63b08e7fcd45896deb8465cb2ea2d0923966e37
truzvng0418 <quoctrung0418@gmail.com>
2026-07-06 20:04:26 +0700

    Delete IPairingService.java

--- IPairingService.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-
-public interface IPairingService {
-    List<Teapot> suggestTeapotsForTea(Tea tea, List<Teapot> teapots);
-    boolean isCompatible(Tea tea, Teapot teapot);
-    String explainPairingRule(Tea tea, Teapot teapot);
-}
