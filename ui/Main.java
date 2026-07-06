840f1788d5b1a611ee093db37910a9938e1c7858
huydts101-star <huydts101@gmail.com>
2026-07-06 20:49:13 +0700

    Create Main.java

---
+++ ui/Main.java
@@ -0,0 +1,13 @@
+package ui;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("=========================================");
+        System.out.println("   CHƯƠNG TRÌNH QUẢN LÝ TIỆM TRÀ   ");
+        System.out.println("=========================================");
+
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.show();
+    }
+}
