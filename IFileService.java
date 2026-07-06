8154cad8dacf202524f602cdf4dbcd77c48821a0
truzvng0418 <quoctrung0418@gmail.com>
2026-07-06 20:02:41 +0700

    Delete IFileService.java

--- IFileService.java
+++
@@ -1,6 +0,0 @@
-public interface IFileService {
-    boolean saveData(String filePath);
-    boolean loadData(String filePath);
-    boolean backupData(String backupPath);
-    boolean restoreData(String backupPath);
-}
