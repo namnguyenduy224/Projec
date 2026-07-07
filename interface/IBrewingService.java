6405ce42345b48ae324dfb2f6127c432965bd070
truzvng0418 <quoctrung0418@gmail.com>
2026-07-07 22:35:23 +0700

    Update IBrewingService.java

--- interface/IBrewingService.java
+++ interface/IBrewingService.java
@@ -1,7 +1,15 @@
 import java.util.List;
 
 public interface IBrewingService {
-    BrewingRecipe createRecipe(Tea tea, Teapot teapot, int teaGram, int waterMl, int temperature, int timeSecond);
+    BrewingRecipe createRecipe(
+            Tea tea,
+            Teapot teapot,
+            int teaGram,
+            int waterMl,
+            int temperature,
+            int timeSecond
+    );
+
     List<BrewingRecipe> getRecipesByTeaType(TeaType teaType);
     String getBrewingGuide(Tea tea);
     boolean isValidRecipe(BrewingRecipe recipe);
