e3e22927a0f88de42d427390266a12efb6ea639c
huydts101-star <huydts101@gmail.com>
2026-07-10 10:58:20 +0700

    Create Utils.java

---
+++ ui/Utils.java
@@ -0,0 +1,35 @@
+package ui;
+
+public class Utils {
+    public static final String RESET = "\u001B[0m";
+    public static final String BOLD = "\u001B[1m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String RED = "\u001B[31m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+
+    public static void printHeader(String title) {
+        System.out.println("\n" + "=".repeat(70));
+        System.out.println(CYAN + BOLD + "          " + title.toUpperCase() + RESET);
+        System.out.println("=".repeat(70));
+    }
+
+    public static void printFooter() {
+        System.out.println("=".repeat(70));
+    }
+
+    public static void printOption(int num, String text) {
+        System.out.println(BLUE + num + ". " + text + RESET);
+    }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+    }
+
+    public static void printPagination(int current, int total) {
+        System.out.println(YELLOW + "\nTrang " + current + "/" + total + 
+                          " | Nhấn số trang hoặc Enter (tiếp) hoặc 0 (quay lại)" + RESET);
+    }
+}
